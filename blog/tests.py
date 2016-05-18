dd8d162c9101694a0eb16e18bd294ef6a589cbe0
Antonis Kalipetis <akalipetis@gmail.com>
2016-05-18 19:05:35 +0300

    Add test for signal like increment

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from blog import models
 
-# Create your tests here.
+
+class ModelsCase(TestCase):
+    def setUp(self):
+        self.article = models.Article.objects.create(
+            title='New article', text='Demo text', author='42')
+
+    def test_like_increment(self):
+        """
+        Test that creating a like, correctly increments the counter of the
+        article.
+        """
+        models.Like.objects.create(article=self.article)
+        self.article.refresh_from_db()
+        self.assertEqual(self.article.like_count, 1)
